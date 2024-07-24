7464adec9aac6639492f1d236e9d04bfb22339c5
Aviral Chauhan <aviralchauhan@aviral.local>
2024-07-24 17:25:12 +0530

    utils + first insert done

---
+++ server/prisma/migrations/20240724114827_schema_initialisation/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "artists" DROP NOT NULL;
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -5,7 +5,7 @@ import querystring from "querystring";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { errorHandler } from "../utils/errorHandler.js";
 import { responseHandler } from "../utils/responseHandler.js";
-import { error } from "console";
+import prisma from "../../DB/db.config.js";
 
 // Utility function to generate a random string for state
 const generateRandomString = (length) => {
@@ -37,7 +37,7 @@ const callback = asyncHandler(async (req, res) => {
   const code = req.query.code || null;
   const state = req.query.state || null;
 
-  console.log("Authorization code:", code);
+  // console.log("Authorization code:", code);
 
   if (state === null) {
     res.redirect(
@@ -49,51 +49,71 @@ const callback = asyncHandler(async (req, res) => {
     return;
   }
 
-  try {
-    const data = await spotifyApi.authorizationCodeGrant(code);
-    const accessToken = data.body["access_token"];
-    const refreshToken = data.body["refresh_token"];
-    console.log(`The access token is: ${accessToken}`);
-    console.log(`The access token expires in: ${data.body["expires_in"]}`);
-    console.log(`The refresh token is: ${refreshToken}`);
+  const data = await spotifyApi.authorizationCodeGrant(code);
+  const access_Token = data.body["access_token"];
+  const refresh_Token = data.body["refresh_token"];
+  // console.log(`The access token is: ${access_Token}`);
+  // console.log(`The access token expires in: ${data.body["expires_in"]}`);
+  // console.log(`The refresh token is: ${refresh_Token}`);
 
-    spotifyApi.setAccessToken(accessToken);
-    spotifyApi.setRefreshToken(refreshToken);
+  spotifyApi.setAccessToken(access_Token);
+  spotifyApi.setRefreshToken(refresh_Token);
 
-    const userInfo = await spotifyApi.getMe();
-    console.log(`User information: ${JSON.stringify(userInfo.body)}`);
+  const info = await spotifyApi.getMe();
+  const userInfo = info.body;
+  // console.log(JSON.stringify(info));
 
-    // res.redirect(
-    //   "/#" +
-    //     querystring.stringify({
-    //       access_token: accessToken,
-    //       refresh_token: refreshToken,
-    //     })
-    // );
-
-    return res
-      .status(200)
-      .cookie("accessToken", accessToken)
-      .cookie("refreshToken", refreshToken)
-      .json(
-        new responseHandler(
-          200,
-          { userInfo: userInfo.body, accessToken, refreshToken },
-          "User logged in successfully"
-        )
-      );
-  } catch (err) {
-    console.log(
-      "Something went wrong when retrieving the access token:",
-      err.message
-    );
-    res.redirect(
-      "/#" +
-        querystring.stringify({
-          error: "invalid_token",
-        })
-    );
+  const display_name = userInfo.display_name;
+  console.log(display_name);
+  const avatarUrl = userInfo.images[1].url;
+  console.log(avatarUrl);
+  const email = userInfo["email"];
+  console.log(email);
+  var isPremium = false;
+  const product = userInfo.product;
+  if (product === "premium") {
+    isPremium = true;
   }
+  console.log(isPremium);
+
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (findUser) {
+    throw new errorHandler(409, "User with email already exists.");
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      display_name: display_name,
+      avatarUrl: avatarUrl,
+      email: email,
+      isPremium: isPremium,
+      accessToken: access_Token,
+      refreshToken: refresh_Token,
+    },
+  });
+
+  // res.redirect(
+  //   "/#" +
+  //     querystring.stringify({
+  //       access_token: accessToken,
+  //       refresh_token: refreshToken,
+  //     })
+  // );
+
+  return res
+    .status(200)
+    .json(
+      new responseHandler(
+        200,
+        { data: newUser, access_Token, refresh_Token },
+        "User logged in successfully"
+      )
+    );
 });
 
 export { accountAuthorization, callback };
